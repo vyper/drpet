5730c75ebae7570d92bf64c352a9e8f0f75a2f41
Leonardo Saraiva <vyper@maneh.org>
2015-06-24 00:49:25 -0300

    Improvement on controllers spec

--- spec/web/controllers/pets/index_spec.rb
+++ spec/web/controllers/pets/index_spec.rb
@@ -2,11 +2,24 @@ require 'spec_helper'
 require_relative '../../../../apps/web/controllers/pets/index'
 
 describe Web::Controllers::Pets::Index do
+  before do
+    PetRepository.create(Pet.new(name: 'Bacon'))
+    @pets = PetRepository.all
+  end
+
   let(:action) { Web::Controllers::Pets::Index.new }
   let(:params) { Hash[] }
 
-  it "is successful" do
+  it 'is successful' do
     response = action.call(params)
+
     expect(response[0]).to eq 200
+    expect(action.pets).to eq @pets
+  end
+
+  it 'exposures pets' do
+    response = action.call(params)
+
+    expect(action.pets).to eq @pets
   end
 end
