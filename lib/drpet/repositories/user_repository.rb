2ddecf35de9cabddc23ece5b8eafce56f8e73ca3
Leonardo Saraiva <vyper@maneh.org>
2015-07-02 00:37:58 -0300

    Added methods to omniauth on user repository

--- lib/drpet/repositories/user_repository.rb
+++ lib/drpet/repositories/user_repository.rb
@@ -6,4 +6,28 @@ class UserRepository
       where(email: email)
     end.first
   end
+
+  def self.find_by_uid(uid)
+    query do
+      where(uid: uid)
+    end.first
+  end
+
+  def self.find_or_create_from_omniauth(auth)
+    uid   = auth['uid']
+    email = auth['info']['email']
+
+    user = find_by_uid(uid)
+
+    if !user
+      if user = find_by_email(email)
+        user.uid = uid
+        user = persist(user)
+      else
+        user = create(User.new(uid: uid, email: email, password: SecureRandom.hex))
+      end
+    end
+
+    user
+  end
 end
