6594a904051320bca18fea562d9c50c346e702b8
Leonardo Saraiva <vyper@maneh.org>
2015-07-03 00:56:19 -0300

    Applying authenticable module to controllers

---
+++ apps/web/controllers/authenticable.rb
@@ -0,0 +1,25 @@
+module Web
+  module Controllers
+    module Authenticable
+      def self.included(action)
+        action.class_eval do
+          expose :current_user
+        end
+      end
+
+      private
+
+      def authenticate!
+        redirect_to routes.new_user_session_path unless authenticated?
+      end
+
+      def authenticated?
+        !!current_user
+      end
+
+      def current_user
+        @current_user ||= UserRepository.find(session[:logged_user_id])
+      end
+    end
+  end
+end
