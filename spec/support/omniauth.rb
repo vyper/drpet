8945612787967d025d2ba877c3c361065df2d7cb
Leonardo Saraiva <vyper@maneh.org>
2015-07-06 01:34:02 -0300

    Added features for authencation

--- spec/support/omniauth.rb
+++ spec/support/omniauth.rb
@@ -10,10 +10,20 @@ module Omniauth
       )
     end
   end
+
+  module SessionHelpers
+    def sign_in(user)
+      visit '/login'
+      expect(page).to have_content('using facebook')
+      auth_mock(user)
+      click_link 'using facebook'
+    end
+  end
 end
 
 RSpec.configure do |config|
   config.include Omniauth::Mock
+  config.include Omniauth::SessionHelpers, type: :feature
 end
 
 OmniAuth.config.test_mode = true
