7bb4fa23175084fe65d5256a3abae8c4fb9cef4e
Leonardo Saraiva <vyper@maneh.org>
2015-07-07 22:05:02 -0300

    Fixing api routes (:

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,6 @@ require_relative '../apps/api/application'
 require_relative '../apps/web/application'
 
 Lotus::Container.configure do
-  # mount Api::Application, at: '/'
+  mount Api::Application, at: '/api/v1'
   mount Web::Application, at: '/'
 end
