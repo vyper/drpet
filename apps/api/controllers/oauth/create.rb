cd7162929a2c3fca1a76ab038ee28a5c323fc30a
Leonardo Saraiva <vyper@maneh.org>
2015-07-18 17:35:25 -0300

    Added specs for controller oauth#create

--- apps/api/controllers/oauth/create.rb
+++ apps/api/controllers/oauth/create.rb
@@ -13,11 +13,24 @@ module Api::Controllers::Oauth
     end
 
     def call(params)
-      byebug
       redirect_to '/' unless params.valid? # TODO: Create a page for error
 
-      @client_app = ClientAppRepository.find_by_app_id(params[:client_id])
-      redirect_to '/' unless @client_app # TODO: Create a page for error
+      client_app = ClientAppRepository.find_by_app_id(client_app_params.get('app_id'))
+      redirect_to '/' unless client_app # TODO: Create a page for error
+
+      auth_grant = AuthGrantRepository.find_or_create_by_client_app_id_and_user_id(client_app.id, current_user.id)
+      # TODO Ouch! Move to the interactor, please!
+      redirect_to_url = client_app_params.get('redirect_uri')
+      divisor = (redirect_to_url =~ /\?/) ? '&' : '?'
+      redirect_to_url = "#{redirect_to_url}#{divisor}code=#{auth_grant.code}&response_type=code"
+
+      redirect_to redirect_to_url
+    end
+
+    private
+
+    def client_app_params
+      params.get('client_app')
     end
   end
 end
---
+++ spec/drpet/interactors/auth_grant_creator_spec.rb
@@ -0,0 +1,4 @@
+# require 'spec_helper'
+
+# describe AuthGrantCreator do
+# end
