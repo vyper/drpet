72e16a0c13e26a90d4523a02b321f438ab646973
Leonardo Saraiva <vyper@maneh.org>
2015-07-18 18:33:51 -0300

    Added feature spec for oauth#new

--- spec/api/features/oauth_spec.rb
+++ spec/api/features/oauth_spec.rb
@@ -1,4 +1,42 @@
 require 'features_helper'
 
 RSpec.describe 'OAuth' do
+  let!(:user) { UserRepository.create(User.new(uid: '1234', email: 'leo@nospam.org', password: '123456')) }
+  let!(:client_app) { ClientAppRepository.create(ClientApp.new(name: 'nome da aplicação', app_id: 'app_id', app_secret: 'app_secret', permissions: 'permissions', redirect_uri: 'http://localhost', user_id: user.id)) }
+
+  context 'unlogged user' do
+    it 'GET #new' do
+      visit "/api/oauth/new?app_id=#{client_app.app_id}"
+
+      expect(current_path).to eq '/login'
+    end
+  end
+
+  context 'logged user' do
+    before { sign_in(user) }
+
+    context 'GET #new' do
+      it 'invalid param' do
+        visit '/api/oauth/new'
+        expect(current_path).to eq '/'
+      end
+
+      it 'shows client app name' do
+        visit "/api/oauth/new?app_id=#{client_app.app_id}"
+        expect(page.body).to include client_app.name
+      end
+
+      it 'shows button to authorize' do
+        visit "/api/oauth/new?app_id=#{client_app.app_id}"
+
+        expect(page.body).to have_css 'button[type=submit]', text: 'Authorize this application'
+      end
+    end
+  end
+
+  after do
+    PetRepository.clear
+    ClientAppRepository.clear
+    UserRepository.clear
+  end
 end
