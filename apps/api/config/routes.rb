53eb12f136cdf3e241050f43f6ae648a1550f633
Leonardo Saraiva <vyper@maneh.org>
2015-07-19 00:38:14 -0300

    Added action to request access token

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -4,6 +4,6 @@ namespace :v1 do
   resources :pets, only: :index
 end
 
-get  '/oauth/new',       to: 'oauth#new',    as: :oauth_new
-post '/oauth/authorize', to: 'oauth#create', as: :oauth_authorize
-# post '/oauth/token',     to: 'oauth#token',  as: :oauth_token
+get  '/oauth/new',          to: 'oauth#new',          as: :oauth_new
+post '/oauth/authorize',    to: 'oauth#create',       as: :oauth_authorize
+post '/oauth/access_token', to: 'oauth#access_token', as: :oauth_access_token
