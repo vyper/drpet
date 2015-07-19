717e94d843e1e9e272b6ef57d2086c9290ac3d61
Leonardo Saraiva <vyper@maneh.org>
2015-07-19 20:40:09 -0300

    Added support to oauth token in `authenticate!`

--- lib/extensions/controllers/authenticable.rb
+++ lib/extensions/controllers/authenticable.rb
@@ -21,8 +21,13 @@ module Extensions
       def authenticate!
         unless authenticated?
           session[:redirect_to] = request.env['REQUEST_URI'] if request.env['REQUEST_URI']
+
           # TODO: Correct way to access routes in other app?
-          redirect_to '/login'
+          if format == :json
+            halt 401
+          else
+            redirect_to '/login'
+          end
         end
       end
 
@@ -35,7 +40,25 @@ module Extensions
       end
 
       def current_user
-        @current_user ||= UserRepository.find(session['logged_user_id'])
+        @current_user ||= begin
+          user_id = auth_grant ? auth_grant.user_id : session['logged_user_id']
+          @current_user ||= UserRepository.find(user_id)
+        end
+      end
+
+      def auth_grant
+        if oauth_access_token
+          @auth_grant ||= AuthGrantRepository.find_by_access_token(oauth_access_token)
+        end
+      end
+
+      def oauth_access_token
+        auth  = request.env['HTTP_AUTHORIZATION'] || ''
+        match = auth.match(/token\W*([^\W]*)/)
+
+        return match[1] if match
+
+        nil
       end
     end
   end
