33ce7676850f42d40e157d51a8e64bf8aa8e66b0
Leonardo Saraiva <vyper@maneh.org>
2015-07-27 00:05:05 -0300

    Change create and update actions to use interactor

--- apps/web/controllers/pets/create.rb
+++ apps/web/controllers/pets/create.rb
@@ -7,26 +7,19 @@ module Web::Controllers::Pets
     # TODO Move to other class
     params do
       param :pet do
-        param :name, type: String, presence: true
+        param :name, presence: true
       end
     end
 
     expose :pet
 
     def call(params)
-      @pet = Pet.new pet_params
-      @pet.user_id = current_user.id # TODO Improve this
+      result = PetPersistor.new(params, current_user).call
+      @pet = result.pet
 
-      if params.valid?
-        PetRepository.create(@pet)
+      if result.success?
         redirect_to routes.pets_path
       end
-    end
-
-    private
-
-    def pet_params
-      params[:pet]
     end
   end
 end
